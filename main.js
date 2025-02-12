e906b23aa9bc5f69c6136f47bbde9f98422a75d0
mert00731 <mertsiyami.03@gmail.com>
2025-02-12 12:28:13 +0300

    https to http

--- main.js
+++ main.js
@@ -18,6 +18,17 @@ connectDB()
 // Express
 const app = express()
 
+// HTTPS yönlendirmesini kaldır
+app.disable('trust proxy');
+
+// HTTP üzerinden çalıştır
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    return res.redirect(307, `http://${req.headers.host}${req.url}`);
+  }
+  next();
+});
+
 // Middlewares
 app.use(bodyParser.json())
 app.use(cors())
