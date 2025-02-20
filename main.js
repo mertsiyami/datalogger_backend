8b6aceff74fde119bfb0a2373bbdb3c987b4558b
mert00731 <mertsiyami.03@gmail.com>
2025-02-20 18:23:27 +0300

    cors changes

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 // Middlewares
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({ origin: "https://datalogger-frontend-74fcd5ae475e.herokuapp.com", credentials: true }))
 
 // Routes
 app.use('/user'  , userRoutes)
