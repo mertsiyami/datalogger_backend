df8899ab8f0228ad449b30a20f545e6b6c6fc872
mert00731 <mertsiyami.03@gmail.com>
2025-02-20 18:26:38 +0300

    cancel small change

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 // Middlewares
 app.use(bodyParser.json())
-app.use(cors({ origin: "https://datalogger-frontend-74fcd5ae475e.herokuapp.com", credentials: true }))
+app.use(cors())
 
 // Routes
 app.use('/user'  , userRoutes)
